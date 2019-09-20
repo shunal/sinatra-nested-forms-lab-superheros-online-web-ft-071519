1348ae2baedfd45ad739ed46233b3794dfc638ce
Steven Hunal <stevehunal@gmail.com>
2019-09-20 04:01:44 +0000

    Automatically backed up by Learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,16 +1,16 @@
 class Hero
   
   attr_accessor :name, :power, :bio
-  HEROES = []
+  @@all = []
   def intialize(params)
     @name = params[:name]
     @power = params[:power]
     @bio = params[:bio]
-    HEROES << self
+    @@all << self
   end 
   
   def self.all
-    HEROES
+    @@all
   end 
   
 end 
