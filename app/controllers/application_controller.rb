fc1a941e8874b39586232648e75c1ac43bd2be5a
Steven Hunal <stevehunal@gmail.com>
2019-09-20 04:21:08 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,20 +11,8 @@ end
 post "/teams" do 
  @team_name= params[:team][:name]
  @team_motto= params[:team][:motto]
- @hero_names= []
- @hero_power= []
- @hero_bio= []
  @team_members= params[:team][:members]
- @team_members.each do |member|
-   @hero_names << member[:name]
-   @hero_power << member[:power]
-   @hero_bio << member[:bio]
- end
-   
- end 
-   
-   
-  binding.pry
+ 
   erb :show
 end 
 
