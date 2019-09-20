ec13253dee12802118d84f0a06d39b278a9bad26
Steven Hunal <stevehunal@gmail.com>
2019-09-20 03:59:04 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,21 +9,20 @@ get "/" do
 end
 
 post "/teams" do 
-  @team = Team.new(params[:team])
-  params[:team][:heroes].each do |details|
-    Hero.new(details)
-  end
-    @heroes= Hero.all
-  
-  erb :show
-end 
-
-
-end
-@team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
+  # @team = Team.new(params[:team])
+  # params[:team][:heroes].each do |details|
+  #   Hero.new(details)
+  # end
+  #   @heroes= Hero.all
+  @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
       members = params[:team][:members]
 
       members.each do |member_params|
         Hero.new({name: member_params[:name], power: member_params[:power], bio: member_params[:bio]})
       end
-      @super_heroes = SuperHero.all
+      @heroes = SuperHero.all
+  erb :show
+end 
+
+
+end
