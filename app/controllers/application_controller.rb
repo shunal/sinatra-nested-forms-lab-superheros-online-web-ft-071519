67255b902ec692160009ac85a81eb5a4ba359589
Steven Hunal <stevehunal@gmail.com>
2019-09-20 03:50:43 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 require 'sinatra/base'
-
+require 'pry'
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
@@ -10,7 +10,7 @@ end
 
 post "/teams" do 
   @team = Team.new(params[:team])
-  
+  binding.pry
   params[:team][:heroes].each do |details|
     Hero.new(details)
   end
